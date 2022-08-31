6f19c722409b4fd46b197888896684982584d9bb
ChieuAnh-Nguyen <chieuanh.nguyen@sjsu.edu>
2022-08-30 17:11:42 -0700

    added a layout template

--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
 
+from flask import Flask, render_template
+
+
+posts = [
+    {
+        "user": "Bob",
+        "message": "hey"
+
+    },
+    {
+        "user": "Mary",
+        "message": "sup"
+    }
+]
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/home")
+
+# about.html uses the posts variable on the left, which is assigned posts created up top'''
+def home():
+    return render_template("layout.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html", posts = posts)
+
+if __name__ == '__main__':
+    app.run(debug=True)
